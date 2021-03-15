a79b879b67aeff447f8be745db231e43497bc9f7
thusay <thusay27@gmail.com>
2021-03-15 22:09:41 +0700

    khoi tao lan dau

---
+++ index.js
@@ -0,0 +1,291 @@
+// // let inventory ={
+// //     gold: 500,pouch :['flint', 'twine','gemstone'],
+// //     backpack: ['xylophone','dagger','bedroll','bread loaf']}
+// // inventory.pocket = ['seashell','strange','lint']
+// // inventory.backpack.splice(1,1);
+// // inventory.gold +=50;
+// // delete inventory.pouch;
+// // console.log(inventory);
+
+
+
+// // let a=5
+
+// // function myFunction(){
+// //     console.log('ni hao')
+// //     console.log('xi huan')
+// //     console.log('jing tian')
+// //     console.log(a)
+// // }
+// // myFunction();
+
+// // function add(a,b){
+// //     c = a + b
+// //     console.log(c)
+// // }
+// // add(10,11)
+// // //  a, b được gọi là parameter(),  10,11 là argurment(cái truyền vào)
+
+// // function add(a,b){
+// //     c = a + b
+// //     return c
+// // }
+// // let result = add(20,40)
+// // console.log(result)
+
+// // let myPenguin = {
+// //     name: ['Aptenodytes forsteri'],
+// //     hight: 122,
+// //     weight: 22,
+// //     ability: ['eat', 'drink']
+// // }
+// // console.log(myPenguin.name);
+// // alert('Hello, I am a penguin and my name is ' + myPenguin.name);
+// // // myPenguin.ability.push('can fly')
+// // myPenguin.canFly = false
+
+// // myPenguin.chirp = 'CHIRP CHIRP! Is this what penguins sound like?'
+// // console.log(myPenguin.chirp);
+
+// // myPenguin.chipchip = function () {
+// //     console.log('chip chip')
+// // }
+
+// // myPenguin.chipchip();
+
+// // myPenguin.sayHello = function () {
+// //     console.log('hello, Iam a penguin and my name is  ' + this.name)
+// // }
+// // myPenguin.name = 'chrischris'
+// // myPenguin.sayHello()
+
+// // myPenguin.fly = function () {
+// //     if (this.canfly == true) {
+// //         console.log('i can fly')
+// //     }
+// //     else {
+// //         console.log('i cant fly')
+// //     }
+// // }
+
+// // myPenguin.fly()
+// // myPenguin.canFly = true
+// // myPenguin.fly()
+
+// // for (i in myPenguin) {
+// //     console.log(i)
+// // }
+
+
+
+
+// // bài 1.1
+// // const product ={
+// //     name:`Xiaomi rice cooker`,
+// //     price: '1700',
+// //     brand: `Xiaomi`,
+// //     color: `white`
+// // };
+// // for (let x in product){
+// //     console.log(x)
+// // }
+// //  x receives from product is value
+
+// // console.log(product.name, product.price, product.brand, product.color)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // 4.1
+// // alert ('Hi there, this is dev dictionary')
+// // prompt ('enter a keyword')
+
+// // let phones = [{
+// //     name : 'xiaomi portable charger 20000mah',
+// //     brand: 'Xiaomi',
+// //     price:'428',
+// //     color:'white',
+// //     category:'charger'},
+// //     {name:'Vsmart Active 1',
+// //     brand:'Vsmart',
+// //     price:'5487',
+// //     color:'black',
+// //     category:'phone'},
+// //     {name:'Vsmart Active 1',
+// //     brand:'Vsmart',
+// //     price:'5487',
+// //     color:'black',
+// //     category:'phone'},
+// //     {name:'Vsmart Active 1',
+// //     brand:'Vsmart',
+// //     price:'5487',
+// //     color:'black',
+// //     category:'phone'},
+// //     {name: 'iphone X',
+// //     brand:'Apple',
+// //     price:'21490',
+// //     color:'gray',
+// //     category:'phone'},
+// //     {name: 'Samsung Galaxy A9',
+// //     brand:'Samsung',
+// //     price:'8490',
+// //     color:'blue',
+// //     category:'phone'}
+// // ];
+// // console.log(phones);
+
+
+
+
+// let dict = {
+//       debug: `The process of figuring out why your program has a certain error and ho
+//     w to fix it`,
+//       done : `When your task is complete, the only thing you have to do is to wait fo
+//     r users to use it (no additional codes or actions needed)`,
+//       defect: `The formal word for ‘error’`,
+//       pm : `The short version  of Project Manager, the person in charge of the final 
+//     result of a project`,
+// //       "ui/ux" : `UI meanw your products looks and feels`
+// // }
+// //     alert(` Hi there, this is dev dictionary`);
+// //     let key=prompt(`Enter the Keyword`);
+// //     if (dict[key] == undefined){
+// //       alert (`We could not find your word: ${key}`);
+// //     } else {
+// //       alert (key+`\n `+ dict[key]);
+// //     }
+
+
+
+
+
+// //     product[0].Providers =`Phukienzero, Dientuccc`;
+// //     product[1].Providers =`Tgdd, Ddghn, VhStore`;
+// //     product[2].Providers =`Tgdd`;
+// //     product[3].Providers =`Tgdd`;
+// //     for (i=0; i < product.length, i++ ) {
+// //       console.log (`#${i}. ` + product[i].Name);
+// //       console.log (`   Price: `+ product[i].Price);
+// //       console.log (`   Providers: ` + product[i].Providers);
+// //     }
+
+
+
+// // // chữa BÀI TẬP VỀ NHÀ BUỔI 3
+
+
+
+
+
+
+
+// // //  Buổi 4
+
+// // let {debug} = myObj
+// // console(myObj)
+
+
+
+
+
+
+// // let colors = ['red', 'white', 'black', 'green', 'pink']
+// // console.log(colors);
+// // let color1 = colors[0]
+// // console.log(color1);
+// // let color3 = colors[2]
+// // console.log(color3);
+// // console.log(colors[colors.length - 1])
+// // colors[colors.length - 1] = 'ultraviolet'
+// // console.log(colors);
+// // let fourthColor = colors[3];
+// // console.log(fourthColor);
+// // colors.push('grey')
+// // console.log(colors);
+// // colors.unshift('yellow')
+// // console.log(colors);
+// // console.log(colors.length);
+// // colors.pop()
+// // console.log(colors.length);
+// // for (let i = 0; i < colors.length; i++) {
+// //     console.log(colors[i])
+// // };
+// // for (let i = 0; i < colors.length; i++) {
+// //     console.log(i, colors[i])
+// // };
+// // let lastColor = colors[colors.length - 1];
+// // console.log(lastColor);
+
+
+
+// // let person = {
+// //     firstname: 'thusay',
+// //     age: '30',
+// //     lastname: 'tran',
+// //     address: 'hanoi'
+// // };
+// // console.log(person);
+// // console.log(person.firstname, person.lastname)
+// // alert('hello, my name is ' + person.firstname + ' ' + person.lastname)
+
+// // person.iQ = Math.floor(Math.random() * (200 - 120) + 120);
+// // console.log(person.iQ);
+
+// // alert("Hello! It nice to meet you!");
+// // person.sayHello = function () {
+// //     console.log('Hello! It nice to meet you! My name is ' + this.firstname + ' ' + this.lastname)
+// // }
+// // person.sayHello();
+
+
+// // //  câu 6 ?
+
+// // person.firstname = 'Bill Gate'
+// // console.log(person.firstname);
+// // let testIQ = prompt(' IQ của bạn là bao nhiêu?')
+// // if (150 <= testIQ) { console.log('No, i’m not good enough!') } else { console.log('I’m smart!') };
+
+
+// // person.testIQ()
+// // person.iQ = rand();
+// // person.testIQ();
+
+// function check(a) {
+//     if(Array.isArray(a)) {
+//         console.log("True")
+//     } else {console.log("False")}
+// }
+
+
+
+
+// FUNCTION WITH OBJECT:
+
+// let student = {
+//     name: 'David Rayy',
+//     age: '32',
+//     address: 'usa',
+//     rollno : '12'
+// };
+// function student(){console.log(student)}
+
+
+
+
+// BUỔI 5
